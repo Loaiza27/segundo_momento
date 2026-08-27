d96d9af3729908029ae76c5daaac5fb2f152023b
Luis Loaiza <lloaiza.garcia27@gmail.com>
2026-08-27 16:24:55 -0500

    Inicio de for

--- LotesIventarios.java
+++ LotesIventarios.java
@@ -4,10 +4,32 @@ public class LotesIventarios {
 
     
     public static void main(String[] args) {
-        double totalCostos = 0;
-        int productosCategoriaAlta = 0;
+        // DECLARACION DE VARIABLES
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese la cantidad de lotes: ");
+        System.out.print("Ingrese el número de productos: ");
+        int numProductos = scanner.nextInt();
+        System.out.print("Ingrese el nombre del producto: ");
+        String nombreProducto = scanner.next();
+        System.out.print("Ingrese el total de costos: ");
+        double totalCostos = scanner.nextDouble();
+        System.out.print("Cantidad de productos: ");
+        int cantidadProductos = scanner.nextInt();
+        System.out.print("productos categoria alta: ");
+        int productosCategoriaAlta = scanner.nextInt();
+        System.out.print("costo base: ");
+        double costoBase = scanner.nextDouble();
+        System.out.print("costo promedio: ");
+        double costoPromedio = scanner.nextDouble();
+
+        // ENTRADA DE LA CANTIDAD DE LOS PRODUCTOS
+        System.out.print("cuantos productos ingresaran al lote: ");
+        cantidadProductos = scanner.nextInt();
+        scanner.nextLine();
+        // INICIO DE FOR 
+        for (int i =1; i<=cantidadProductos;i++){
+
+        }
+        
     }
 }
     
