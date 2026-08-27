caaa6a9ee16627158b21fa6711a60f264a05a7e0
Luis Loaiza <lloaiza.garcia27@gmail.com>
2026-08-27 14:10:08 -0500

    Creacion de archivo y primeras variables

---
+++ LotesIventarios.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LotesIventarios {
+
+    
+    public static void main(String[] args) {
+        double totalCostos = 0;
+        int productosCategoriaAlta = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de lotes: ");
+    }
+}
+    
